8930f2a51dd7524857cbad10b0185f149e2596a1
Lucian Rezende <lucianfiocello@gmail.com>
2021-06-28 08:22:48 -0300

    Testing commands

---
+++ Store.Tests/Commands/CreateCustomerCommandTests.cs
@@ -0,0 +1,24 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.StoreContext.CustomerCommands.Inputs;
+
+namespace Store.Tests.ValueObjects
+{
+    [TestClass]
+    public class CreateCustomerCommandTests
+    {
+
+        [TestMethod]
+        public void ShouldValidateWhenCommandIsValid()
+        {
+            var command  = new CreateCustomerCommand();
+            command.FirstName = "Joao";
+            command.LastName = "Silva";
+            command.Document = "25198233056";
+            command.Email = "joao@email.com";
+            command.Phone = "11999992222";
+            
+            Assert.AreEqual(true, command.Valid());
+        }
+    }
+}
